d1499b4dc6ffb73bb1ed037279e80eba912e5cb1
Shanmugarajeshwaran R <tweets.shan@gmail.com>
2021-05-16 09:07:32 +0530

    Initial Commit

---
+++ Toggle-Switch-With-Label/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toggle-Switch-With-Label
+//
+//  Created by ADMIN on 16/05/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
