c9ad19d5eb93ebe4e1994dadf86b9abe70bccae3
Shanmugarajeshwaran R <tweets.shan@gmail.com>
2021-05-16 09:15:36 +0530

    Initital commit.

---
+++ Toggle-Switch-With-Label/Controller/LabelSwitch.swift
@@ -0,0 +1,26 @@
+//
+//  LabelSwitch.swift
+//  Toggle-Switch-With-Label
+//
+//  Created by ADMIN on 16/05/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class LabelSwitch: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureView()
+        
+    }
+    // MARK:- Configuration
+    private func configureView() {
+        
+    }
+}
--- Toggle-Switch-With-Label/Controller/ViewController.swift
+++ Toggle-Switch-With-Label/Controller/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lableSwitch: LabelSwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
